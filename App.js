ad35226425374bca853d8fd4a107f03238778523
eesope <81015409+eesope@users.noreply.github.com>
2021-12-26 22:03:32 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, Button, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './navigation/StackNavigator';
+
 import * as  Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
-
-import Home from './components/Home';
-
 
 
 export default function App() {
 
-  const [ready, setReady] = useState(false);
-
-  //initial list of sentences, make array
-  const [list, setList] = useState([]);
-
-  // useEffect(() => console.log(list), [list]);
-
-  // load list from the async storage
-  const loadList = () => {
-    AsyncStorage.getItem('storedList').then(data => {
-      if (data !== null) {
-        setList(JSON.parse(data))
-
-        // console.log(list)
-      }
-    }).catch((error) => console.log(error));
-  }
-
-  if (!ready) {
-    return(
-      <AppLoading
-        startAsync={loadList}
-        onFinish={() => setReady(true)}
-        onError={console.warn}
-      />
-    )
-  }
-
   return (
 
-  <Home 
-  list={list}
-  setList={setList}
-  />
+    <NavigationContainer>
+      <StatusBar style='auto' />
+      <StackNavigator/>
+    </NavigationContainer>
 
   );
 
