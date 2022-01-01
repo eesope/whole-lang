8d4c9cd56d10242a7e6ba9d30fdc757bdafc377e
eesope <81015409+eesope@users.noreply.github.com>
2022-01-01 23:12:11 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,102 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigation/StackNavigator';
 
-import * as  Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
+import * as  Notifications from 'expo-notifications';
+// This refers to the function defined earlier in this guide, in Push Notifications Set Up
+import RegisterForPushNotificationsAsync from './RegisterForPushNotificationsAsync';
+import Home from './components/Home'
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  // for get a user's permission
+  useEffect(() => {
+  registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+  notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+    setNotification(notification);
+  });
+
+  responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+    console.log(response);
+  });
+
+  return () => {
+    Notifications.removeNotificationSubscription(notificationListener.current);
+    Notifications.removeNotificationSubscription(responseListener.current);
+  };
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    return token;
+  }
+
+  const sendMessage = (token) => {
+    fetch('https://example.com/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        expoPushToken,
+        to: token,
+        body: {Home}.list.random,
+        _displayInForegrond: true
+      }),
+    });  
+  }
 
   return (
 
     <NavigationContainer>
       <StatusBar style='auto' />
       <StackNavigator/>
-    </NavigationContainer>
+    </NavigationContainer>    
 
   );
 
-}
+};
